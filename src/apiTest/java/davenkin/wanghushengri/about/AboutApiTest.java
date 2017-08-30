ad34bcfe1bef5b181df251a944f7b4e7869efed7
Yun Teng <davenkin@gmail.com>
2017-08-30 09:11:16 +0800

    fix api test

--- src/apiTest/java/davenkin/wanghushengri/about/AboutApiTest.java
+++ src/apiTest/java/davenkin/wanghushengri/about/AboutApiTest.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertNotNull;
