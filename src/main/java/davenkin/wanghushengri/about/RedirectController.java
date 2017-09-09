0d4cbb3286b886d33f460645de41b2b17188a991
Yun Teng <davenkin@gmail.com>
2017-09-09 21:05:06 +0800

    added redirect

--- src/main/java/davenkin/wanghushengri/about/RedirectController.java
+++ src/main/java/davenkin/wanghushengri/about/RedirectController.java
@@ -2,6 +2,7 @@ package davenkin.wanghushengri.about;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 /**
  * Created by yteng on 9/9/17.
@@ -11,7 +12,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class RedirectController {
 
     @RequestMapping(value = "redirect")
-    public String redirect(){
+    public String redirect(@RequestParam("code") String code) {
+        System.out.println("=====" + code);
         return "redirect:/";
     }
 }
